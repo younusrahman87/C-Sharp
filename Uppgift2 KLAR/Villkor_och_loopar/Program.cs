6faf22df17ba136ad32b7108ebce0bb90ee690dd
younusrahman87 <70640104+younusrahman87@users.noreply.github.com>
2020-09-18 08:58:38 +0200

    NU Klart

---
+++ Uppgift2 KLAR/Villkor_och_loopar/Program.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Villkor_och_loopar
+{
+    class Program
+    {
+        static int winner = 0, winner_startnummer = 0, antal_spelare = 0;
+        //------------------------------------------------------------HÄR PRINTER UT TILL KONSOLE-------------------------------------------------------------
+        static void Printer ()
+        {
+            if (antal_spelare > 0)
+            {
+                Console.Clear();
+                int hour = winner / 3600;
+                int min = (winner % 3600) / 60;
+                int latest_sec = winner % 60;
+
+                Console.WriteLine($"::. Vinnare startnr är: {winner_startnummer} .::\n" +
+                                    $"::. {hour} : Timmar, {min} : Minuter, {latest_sec} : Sekunder .::\n" +
+                                      $"::. Antal tävlande: {antal_spelare} .::");
+            }
+            else
+            {
+                Console.WriteLine("::. Inga tävlande .:: ");
+            }
+
+
+        }
+
+        //-------------------------------------------------------------------------RÄKNAR TID TILL SEKUND OCH RÄNKAR DAG------------------------------------------------
+
+        static void Calculate_time(int start_timmer, int start_minut, int start_sekund, int slut_timmer, int slut_minut, int slut_seckund, int startstartnummer)
+        {
+            if (start_timmer < slut_timmer)
+            {
+                int day1_sec = (start_timmer * 3600) + (start_minut * 60) + start_sekund;
+                int day2_sec = (slut_timmer * 3600) + (slut_minut * 60) + slut_seckund;
+                int diff2 = day2_sec - day1_sec;
+                Calcul_winner(diff2, startstartnummer);
+
+            }
+            else
+            {
+                int day1_sec = (start_timmer * 3600) + (start_minut * 60) + start_sekund;
+                int diff1 = 86400 - day1_sec;
+
+                int day2_sec = (slut_timmer * 3600) + (slut_minut * 60) + slut_seckund;
+                int diff2 = diff1 + day2_sec;
+                Calcul_winner(diff2, startstartnummer);
+            }
+        }
+
+        //-------------------------------------------------------------------------¨JÄMFÖR TID FÖRATT HITTA VINNARE------------------------------------------------
+        static void Calcul_winner(int next_runtime, int next_startnummer)
+        {
+            
+            if (winner != 0 && winner < next_runtime)
+            {
+                Console.WriteLine("jag är här");
+            }
+            else
+            {
+                winner_startnummer = next_startnummer;
+
+               winner = next_runtime;
+            }
+
+        }
+
+        //------------------------------------------------------------------------------START MENU-------------------------------------------
+        static void Main(string[] args)
+        {            
+
+            Console.Write("Ange startnummer: ");
+            int startstartnummer = Convert.ToInt32(Console.ReadLine());
+ 
+            while (startstartnummer > 0)
+            {
+                antal_spelare += 1;
+                Console.Write("Ange timme för start: ");
+                int start_timmer = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ange minut för start: ");
+                int start_minut = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ange sekund för start: ");
+                int start_sekund = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ange timme för mål: ");
+                int slut_timmer = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ange minut för mål: ");
+                int slut_minut = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ange sekund för mål: ");
+                int slut_seckund = Convert.ToInt32(Console.ReadLine());
+                //-------------------------------------------------------------------------------------------------------------------------
+                Calculate_time(start_timmer, start_minut, start_sekund, slut_timmer, slut_minut, slut_seckund, startstartnummer);
+                //-------------------------------------------------------------------------------------------------------------------------
+                Console.Clear();
+                Console.Write("Ange nästa startnummer: ");
+                startstartnummer = Convert.ToInt32(Console.ReadLine());
+            }
+            Printer();
+
+        }
+    }
+}
--- Uppgift3/Klasser/Program.cs
+++ Uppgift3/Klasser/Program.cs
@@ -1,23 +1,151 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Klasser
 {
+    
+
     class Program
     {
-        /// <summary>
-        /// Se instruktionenr i Uppgift.txt
-        /// </summary>
-        /// <param name="args"></param>
+        static Program()
+        {
+            Console.WriteLine("Välkomen till YR - Köp/Sälja Bil!\n");
+            Console.WriteLine("[1] Registera en bil\n[2] Söka bil info\n[3] Stänga programmet");
+
+            Console.Write("Välja ett nr : ");
+            string menu_val = Console.ReadLine();
+            bool continue_prog = true;
+
+            while (continue_prog)
+            {
+
+                if (menu_val == "1")
+                {
+                    Register_bil.Reg_menu();
+                }
+                else if (menu_val == "2")
+                {
+                    Console.WriteLine("2");
+                    break;
+                }
+                else if (menu_val == "3")
+                {
+                    Console.WriteLine("3");
+
+                    continue_prog = false;
+
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Fel val försök igen");
+                    Console.WriteLine("Välkomen till YR - Köp/Sälja Bil!\n");
+                    Console.WriteLine("[1] Registera en bil\n[2] Söka bil info\n[3] Stänga programmet");
+                    Console.Write("Välja ett nr : ");
+                    menu_val = Console.ReadLine();
+                }
+            }
+
+
+        }
+        public Dictionary<string, Register_bil> car_database = new Dictionary<string, Register_bil>();
         static void Main(string[] args)
         {
+            Program menu = new Program();
+            
 
-            Console.WriteLine("Hello World!");
-        }
+        }            
     }
 
-    public class Bil
+    class Register_bil
     {
-        public string Registreringsnummer { get; set; }
+        
+        private DateTime registrerades;
+        private string Logo;
+        private bool Elbil;
+        private string Modell { get; set; }
+        private string Reg_nummer { get; set; }
+        private int Vikt { get; set; }
+        public static void Reg_menu ()
+        {
+            Console.Clear();
+
+            Console.Write("Vill du lägga till ny bil. (Ja / Nej) : ");
+            string user_ans = Console.ReadLine();
+
+            bool keep_rolling = true;
+
+            while (keep_rolling)
+            {
+
+                if (user_ans.ToUpper() == "JA")
+                {
+                    string logo, model, regnummer, el_or_not;
+                    int vikt;
+
+                    DateTime registrerades = DateTime.Now;
+
+                    Console.Write("Bil Märke : ");
+                    logo = Console.ReadLine();
+
+                    Console.Write("Bil model : ");
+                    model = Console.ReadLine();
+
+                    Console.Write("Bil Regnummer : ");
+                    regnummer = Console.ReadLine();
+
+                    Console.Write("Bil Vikt : ");
+                    vikt = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("El Bil (ja eller nej) : ");
+                    el_or_not = Console.ReadLine();
+
+                    Register_bil car = new Register_bil();
+
+                    car.Ge_value(registrerades, logo, model, regnummer, vikt, el_or_not);
+
+
+                    Console.Write("Vill du lägga till ny bil. (Ja / Nej) : ");
+                    user_ans = Console.ReadLine();
+                }
+                else if (user_ans.ToUpper() == "NEJ" )
+                {
+                    keep_rolling = false;
+                    break()
+                }
+            }
+            Program menu = new Program();
+           
+
+
+        }
+
+
+        private void Ge_value(DateTime datum, string logo, string modell, string reg_nummer, int vikt, string el_or_not )
+        {
+
+            this.registrerades = datum;
+            this.Logo = logo;
+            this.Modell = modell;
+            this.Reg_nummer = reg_nummer;
+            this.Vikt = vikt;
+
+            if (el_or_not.ToLower() == "yes")
+            {
+                this.Elbil = true;
+            }
+            else
+            {
+                this.Elbil = false;
+            }
+        }
+
+
 
     }
+    
+    
+
+
 }
